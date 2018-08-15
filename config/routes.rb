93cacb5748ab1e932d8479f9e2ca7ea2be6940c3
Daniel Meißner <meise@koeln.ccc.de>
2018-08-15 13:47:30 +0200

    added root and dashboard views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resource :dashboard, :only => :show, :controller => :dashboard
+
+  root :to => 'root#show'
+
 end
