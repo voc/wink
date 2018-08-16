4301c6bd5883390529ec616fa6236261ce93cd2e
Daniel Meißner <meise@koeln.ccc.de>
2018-08-16 15:39:39 +0200

    event: validate attributes

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,8 @@
 class Event < ActiveRecord::Base
   has_many :cases
 
+  validates :name, uniqueness: true
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :location, presence: true
 end
