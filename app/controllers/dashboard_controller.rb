8d1ec8dd64f47e11041eb77f69136b062c131894
Daniel Meißner <meise@koeln.ccc.de>
2018-08-16 18:20:42 +0200

    added page navigation for some sites

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,8 +3,8 @@ class DashboardController < ApplicationController
   def show
     @broken_or_missing_items = Item.where("broken = true or missing = true")
     @upcoming_events = Event.
-      where(start_date: Date.today..6.weeks.after).order(:start_date)
+      where(start_date: Date.today..4.weeks.after).order(:start_date)
     @upcoming_transports = Transport.
-      where(pickup_time: Date.today..6.weeks.after).order(:pickup_time)
+      where(pickup_time: Date.today..4.weeks.after).order(:pickup_time)
   end
 end
