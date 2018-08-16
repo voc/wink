2d6fb8c795cc399cf93e56c950b83644a63ac8bd
Daniel Meißner <meise@koeln.ccc.de>
2018-08-16 16:22:29 +0200

    added some color

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,7 @@ class DashboardController < ApplicationController
 
   def show
     @broken_or_missing_items = Item.where("broken = true or missing = true")
+    @upcoming_events = Event.where(start_date: Date.today..2.month.after)
   end
 
 end
