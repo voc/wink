40cbb86a3203b834982ba988c43bf6e005231621
Daniel Meißner <meise@koeln.ccc.de>
2018-08-23 14:25:46 +0200

    sql: be sqlite compatible

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,7 @@
 class DashboardController < ApplicationController
 
   def show
-    @broken_or_missing_items = Item.where("broken = true or missing = true")
+    @broken_or_missing_items = Item.where(broken: true) + Item.where(missing: true)
     @upcoming_events = Event.
       where(start_date: Date.today..4.weeks.after).order(:start_date)
     @upcoming_transports = Transport.
