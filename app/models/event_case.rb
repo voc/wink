0dbac1e83718a144433f5962de5112c074175084
Daniel Meißner <meise@koeln.ccc.de>
2018-08-24 23:45:57 +0200

    don't destroy event cases with transport or check list defined

--- app/models/event_case.rb
+++ app/models/event_case.rb
@@ -2,6 +2,18 @@ class EventCase < ActiveRecord::Base
   belongs_to :event
   belongs_to :case
   belongs_to :check_list, optional: true
-
   belongs_to :transport, optional: true
+
+  before_destroy :check_for_list_and_transport
+
+
+  private
+
+ # Don't remove EventCases when a transport or a
+ # CheckList exists.
+  def check_for_list_and_transport
+    if self.check_list.nil? || self.transport.nil?
+      return false
+    end
+  end
 end
