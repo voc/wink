190d26aa79c66f04a72d998c7b6c5e518f00ff43
Daniel Meißner <meise@koeln.ccc.de>
2018-08-24 23:48:19 +0200

    missed migration file

---
+++ db/migrate/20180824203922_create_schema_changes.rb
@@ -0,0 +1,10 @@
+class CreateSchemaChanges < ActiveRecord::Migration[5.2]
+  def change
+    change_table(:event_cases) do |t|
+      t.belongs_to :check_list, index: true
+    end
+
+    remove_column :check_lists, :case_id
+    remove_column :check_lists, :event_id
+  end
+end
