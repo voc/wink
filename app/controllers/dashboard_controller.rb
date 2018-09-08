e4f1afd4fad17b4e3e96e60bf8f44c3f0c97caa6
Andreas Hubel <andi@saerdnaer.de>
2018-09-08 15:39:53 +0100

    Dashboard: also show recent events, not only future

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,7 +3,7 @@ class DashboardController < ApplicationController
   def show
     @broken_or_missing_items = Item.where(broken: true) + Item.where(missing: true)
     @upcoming_events = Event.
-      where(start_date: Date.today..4.weeks.after).order(:start_date)
+      where(end_date: Date.today-2..4.weeks.after).order(:start_date)
     @upcoming_transports = Transport.
       where(pickup_time: Date.today..4.weeks.after).order(:pickup_time)
   end
