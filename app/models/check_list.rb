71f88cdea5c78e8f2c69a1f733b35f777e1bc9e0
Daniel Meißner <meise@koeln.ccc.de>
2018-09-09 18:02:34 +0200

    checklist: copy checklist items during creation

--- app/models/check_list.rb
+++ app/models/check_list.rb
@@ -1,5 +1,25 @@
 class CheckList < ActiveRecord::Base
-  belongs_to :event_case
+  has_one :event_case
+  has_one :event, through: :event_case
+  has_one :case,  through: :event_case
 
   has_many :check_list_items
+
+  validates :advisor, presence: true
+
+
+  def copy_items!
+    return false if check_list_items.count > 0
+
+    event_case.case.items.each do |ec_item|
+      cl_item = CheckListItem.new
+      cl_item.item = ec_item
+      cl_item.item.broken = ec_item.broken
+      cl_item.item.missing = ec_item.missing
+      self.check_list_items << cl_item
+      self.save!
+    end
+
+    true
+  end
 end
