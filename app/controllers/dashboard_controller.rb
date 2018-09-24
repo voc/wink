c951b0e4d0caa3e3945b8e1e08064704d48a4d06
Daniel Meißner <meise@koeln.ccc.de>
2018-09-25 01:31:15 +0200

    items: don't show deleted items in dashboard and cases view

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,7 @@
 class DashboardController < ApplicationController
 
   def show
-    @broken_or_missing_items = Item.where(broken: true) + Item.where(missing: true)
+    @broken_or_missing_items = Item.where(broken: true).where(deleted: false) + Item.where(missing: true).where(deleted: false)
     @upcoming_events = Event.
       where(end_date: Date.today-2..4.weeks.after).order(:start_date)
     @upcoming_transports = Transport.
