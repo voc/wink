e5f22cd8056c943d00a5c5bf1a596c653d203fdf
Daniel Meißner <meise@koeln.ccc.de>
2018-10-09 16:31:10 +0200

    mqtt: create config from template when file don't exists

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require_relative 'boot'
 
 require 'rails/all'
+require 'pp'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -19,11 +20,17 @@ module Wink
 
     # il8n config
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}').to_s]
+
     config.i18n.default_locale = :de
 
     # set the application name and slogan
     config.application_name = 'WINK'
     config.application_slogan = '(W)o (i)st mei(n)e Winke(k)atze?'
+
+    unless Pathname(Rails.root + 'config/mqtt.yml').exist?
+      FileUtils.cp(Pathname(Rails.root + 'config/mqtt.yml.template'), Pathname(Rails.root + 'config/mqtt.yml'))
+      puts "Copied #{Pathname(Rails.root + 'config/mqtt.yml.template')} to #{Pathname(Rails.root + 'config/mqtt.yml.template')}"
+    end
 
     config.mqtt = config_for(:mqtt)
 
