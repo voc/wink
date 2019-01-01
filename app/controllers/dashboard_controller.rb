746620beff4fef338128f9bc7e8fa95d6f67c2cc
Andreas Hubel <andi@saerdnaer.de>
2019-01-01 18:23:42 +0100

    group missing/broken items in dashboard by case

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,9 @@ class DashboardController < ApplicationController
 
   def show
     @broken_or_missing_items = Item.where(broken: true).where(deleted: false) + Item.where(missing: true).where(deleted: false)
+    @grouped_items = @broken_or_missing_items.group_by do |i|
+      i.case
+    end
     @upcoming_events = Event.
       where(end_date: Date.today-2..4.weeks.after).order(:start_date)
     @upcoming_transports = Transport.
