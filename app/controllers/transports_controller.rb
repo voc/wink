e107e60847a9ac0b90dc729f9735a39faeeb8ca5
Andreas Hubel <andi@saerdnaer.de>
2019-09-06 02:10:27 +0200

    transports: second step

--- app/controllers/transports_controller.rb
+++ app/controllers/transports_controller.rb
@@ -9,10 +9,36 @@ class TransportsController < ApplicationController
     @transports = Transport.all
   end
 
+  def new
+    @transport = Transport.new
+  end
+
+  def create
+    @transport = Transport.new(transport_params)
+
+    if @transport.save
+      redirect_to edit_transport_path
+    else
+      render action: 'new'
+    end
+  end
+
+  def update
+    if @transport.update_attributes(transport_params)
+      redirect_to transports_path
+    else
+      render action: 'edit'
+    end
+  end
+
   private
 
   def find_transport
     @transport = Transport.find(params[:id])
+  end
+
+  def transport_params
+    params.require(:transport).permit(:source_event_id, :source_address, :destination_event_id, :destination_address)
   end
 
 end
