ae176f6e0d8db8e53dabaea4963da624bd2e6d9b
mwiegand <martin.wiegand@seibert.group>
2025-10-02 20:48:48 +0200

    user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  validates :email, presence: true, uniqueness: true
+  validates :name, presence: true
+  validates :provider, presence: true
+  validates :uid, presence: true, uniqueness: { scope: :provider }
 end
