0af307d70a557c07579b0b68906aa33da9a0810d
Dario Mehlich <d.mehlich@gmail.com>
2025-10-12 14:26:15 +0200

    removed hard-coded relative URL root for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,6 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  config.relative_url_root = "/wink"
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
